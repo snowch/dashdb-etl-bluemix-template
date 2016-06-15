e40620ac14164381d09743e8e82584f9c0cc7cd9
Chris Snow <chsnow123@gmail.com>
2016-06-15 22:27:34 +0100

    Update env_vars.sh

--- env_vars.sh
+++ env_vars.sh
@@ -5,7 +5,12 @@
 export db_username=''
 export db_password=''
 export db_hostname=''
+
+# non-ssl connection
 export db_url="jdbc:db2://${db_hostname}:50000/BLUDB"
+
+# ssl connection
+#export db_url="jdbc:db2://${db_hostname}:50001/BLUDB:sslConnection=true;"
 
 # the name of the table in dashDB createdb by the SDP
 export source_tablename=''
